c7b20782d1fa7e71302d0ff23c18b9ef9458ffd8
Ariel Fonticiella <arielfonticiella@Ariels-MacBook-Pro.local>
2017-12-11 18:59:41 -0500

    first commit

---
+++ models/gifted-models.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const giftedSchema = new Schema(
+  {
+      name: {
+        type: String,
+        required: [true, "Name of person you are gifting is required."]
+      },
+      relationship: {
+        type: String,
+      },
+      interest: [
+        { type: String }
+      ],
+      priceRange: {
+        type: Number,
+        required: [true, "Need price range for gift."]
+      },
+      giftLink: {
+        type: String,
+        required: [true, "Need link to purchase gift."]
+      }
+  },
+
+  {
+    timestamps: true
+  }
+
+);
+
+const Gifted = mongoose.model("Gift", giftedSchema);
+
+module.exports = Gifted;
---
+++ routes/gifted-api-router.js
@@ -0,0 +1,154 @@
+const express = require("express");
+
+const Gifted = require("../models/gifted-models");
+
+
+const router = express.Router();
+
+
+router.get("/gifted", (req, res, next) => {
+    Gifted
+      .find()
+      .limit(25)
+      .exec()
+      .then((giftedResults) => {
+          // respond with the QUERY RESULTS in the JSON format
+          res.status(200).json(giftedResults);
+      })
+      .catch((err) => {
+          console.log("GET /gifted ERROR!");
+          console.log(err);
+
+          // respond with an ERROR MESSAGE in the JSON format
+          res.status(500).json({ error: "Gifted list database error" });
+      });
+}); // GET /gifted
+
+
+router.post("/gifted", (req, res, next) => {
+    const theGifted = new Gifted({
+        name: req.body.name,
+        relationship: req.body.relationship,
+        interest: req.body.interest,
+        priceRange: req.body.priceRange,
+        giftLink: req.body.giftLink
+    });
+
+    theGifted.save()
+      .then(() => {
+          // respond with the NEW GIFTED in the JSON format
+          res.status(200).json(theGifted);
+      })
+      .catch((err) => {
+          console.log("POST /gifted ERROR!");
+          console.log(err);
+
+          // 400 status code if validation error
+          if (err.errors) {
+              // respond with an VALIDATION ERRORS in the JSON format
+              res.status(400).json(err.errors);
+          }
+          else {
+              // respond with an ERROR MESSAGE in the JSON format
+              res.status(500).json({ error: "Gifted save database error" });
+          }
+      });
+}); // POST /gifted
+
+
+router.get("/gifted/:id", (req, res, next) => {
+    if (req.user === undefined) {
+        res.status(400).json({ error: "Not logged in" });
+        return;
+    }
+
+    Gifted.findById(req.params.id)
+      .then((giftedFromDb) => {
+          // 404 if gifted doesn't exist
+          if (giftedFromDb === null) {
+              // respond with an ERROR MESSAGE in the JSON format
+              res.status(404).json({ error: "Gifted not found" });
+          }
+          else {
+              // respond with the QUERY RESULTS in the JSON format
+              res.status(200).json(giftedFromDb);
+          }
+      })
+      .catch((err) => {
+          console.log("GET /gifted/:id ERROR!");
+          console.log(err);
+
+          // respond with an ERROR MESSAGE in the JSON format
+          res.status(500).json({ error: "Gifted details database error" });
+      });
+}); // GET /gifted/:id
+
+
+router.delete("/gifted/:id", (req, res, next) => {
+    if (req.user === undefined) {
+        res.status(400).json({ error: "Not logged in" });
+        return;
+    }
+
+    Gifted.findByIdAndRemove(req.params.id)
+      .then((giftedFromDb) => {
+          if (giftedFromDb === null) {
+              // respond with an ERROR MESSAGE in the JSON format
+              res.status(404).json({ error: "Gifted not found" });
+          }
+          else {
+              // respond with the QUERY RESULTS in the JSON format
+              res.status(200).json(giftedFromDb);
+          }
+      })
+      .catch((err) => {
+          console.log("DELETE /gifted/:id ERROR!");
+          console.log(err);
+
+          // respond with an ERROR MESSAGE in the JSON format
+          res.status(500).json({ error: "Gifted delete database error" });
+      });
+}); // DELETE /gifted/:id
+
+
+router.put("/gifted/:id", (req, res, next) => {
+    Gifted.findById(req.params.id)
+      .then((giftedFromDb) => {
+          if (giftedFromDb === null) {
+              // respond with an ERROR MESSAGE in the JSON format
+              res.status(404).json({ error: "Gifted not found" });
+              return;
+          }
+
+          giftedFromDb.set({
+            name: req.body.name,
+            relationship: req.body.relationship,
+            interest: req.body.interest,
+            priceRange: req.body.priceRange,
+            giftLink: req.body.giftLink
+          });
+
+          return giftedFromDb.save();
+      })
+      .then((giftedFromDb) => {
+          // respond with the QUERY RESULTS in the JSON format
+          res.status(200).json(giftedFromDb);
+      })
+      .catch((err) => {
+          console.log("PUT /gifted/:id ERROR!");
+          console.log(err);
+
+          // 400 status code if validation error
+          if (err.errors) {
+              // respond with an VALIDATION ERRORS in the JSON format
+              res.status(400).json(err.errors);
+          }
+          else {
+              // respond with an ERROR MESSAGE in the JSON format
+              res.status(500).json({ error: "Gifted update database error" });
+          }
+      });
+}); // PUT /gifted/:id
+
+
+module.exports = router;
